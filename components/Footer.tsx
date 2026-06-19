5be8ac794f3070f7a5ea854ff0fbbc785d000029
Devtunechi <tunjidare2@yahoo.com>
2026-06-19 09:19:05 +0100

    Footer syntax

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,6 @@
 export default function Footer() {
   return (
     <footer className="bg-blue-950 text-white px-5 py-10">
-
       <div className="max-w-6xl mx-auto">
 
         <h2 className="font-bold text-xl">
@@ -13,23 +12,27 @@ export default function Footer() {
           through practical technology education.
         </p>
 
-
         <div className="mt-6 text-sm text-gray-300">
 
           <p>
-            Email: info@gnctraininginstitute.com
-          </p>
-
-          <p>
-          Iyana School Bus Stop, 
-          42 Great Challenge Rd, IBA, Lagos 300001, Lagos, Nigeria
-          </p>
-
-          </p>
-          Phone: +234 805 6316 946
+            Phone: +234 805 6316 946
           </p>
 
           <p className="mt-2">
+            Email: info@gnctraininginstitute.com
+          </p>
+
+          <p className="mt-2">
+            Address:
+            <br />
+            Iyana School Bus Stop,
+            <br />
+            42 Great Challenge Rd, IBA,
+            <br />
+            Lagos 300001, Lagos, Nigeria
+          </p>
+
+          <p className="mt-4">
             © {new Date().getFullYear()} Global Net Computer Training Institute.
             All rights reserved.
           </p>
@@ -37,7 +40,6 @@ export default function Footer() {
         </div>
 
       </div>
-
     </footer>
   );
 }
